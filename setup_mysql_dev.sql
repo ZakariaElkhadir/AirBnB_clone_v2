688129000b36a56c9f9da24d1853d7bc59dddc8c
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-19 19:20:38 +0100

    add file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- echo "SHOW DATABASES;" | mysql -uhbnb_test -p | grep hbnb_test_db
+-- echo "SHOW GRANTS FOR 'hbnb_test'@'localhost';" | mysql -uroot -p
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_shema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
