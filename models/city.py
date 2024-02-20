1e170b8a6ffac8a847dc1137a395aebcc8772ffe
root <elmehdibouslam@gmail.com>
2024-02-20 03:07:19 -0800

    Airbnb clone v2

--- models/city.py
+++ models/city.py
@@ -2,10 +2,16 @@
 """
 Defines city Class
 """
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """defines city to look for"""
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan', backref='cities')
