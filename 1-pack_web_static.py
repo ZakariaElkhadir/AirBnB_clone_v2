7dc825b19bb13c72736eafb0ee35798aafccf84d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-09 13:14:45 +0100

    edit file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,11 +9,12 @@ from datetime import datetime
 def do_pack():
     """ Define the archive name with the current timestamp"""
     try:
+        local("mkdir -p versions")
         archive_name = 'web_static_{}.tgz'.format(
             datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
 
-        local('tar -cvzf {} web_static'.format(archive_name))
+        local("tar -cvzf versions/{} web_static".format(archive_name))
 
-        return archive_name
+        return "versions/{}".format(archive_name)
     except Exception as e:
         return None
