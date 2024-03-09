d720651f4284c5ad2d8c064379cf393be77ba16e
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-09 13:22:01 +0100

    edit file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,14 +7,17 @@ from datetime import datetime
 
 
 def do_pack():
-    """ Define the archive name with the current timestamp"""
+    """
+    generates a .tgz archive from the
+    contents of the web_static folder
+    """
     try:
-        local("mkdir -p versions")
-        archive_name = 'web_static_{}.tgz'.format(
-            datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
-
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-
-        return "versions/{}".format(archive_name)
+        local('mkdir -p versions')
+        name = "web_static_{}".format(
+            datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+        local("tar -cvzf versions/{}.tgz {}".format(
+            name, "web_static/"))
+        return "versions/{}".format(name)
     except Exception as e:
         return None
