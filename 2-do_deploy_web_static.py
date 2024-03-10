9694a21bba6d8a4d0ee7820a8ee7c8fb16b66cbf
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-10 15:54:33 +0000

    edit file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Fabric script that distributes
+ an archive to your web servers.
+"""
 import os
 from fabric.api import *
 from datetime import datetime
@@ -25,33 +29,24 @@ def do_deploy(archive_path):
         return False
 
     try:
-        env.hosts = ['<IP web-01>', '<IP web-02>']
-        env.user = '<your_username>'
-        env.key_filename = '<path_to_your_ssh_key>'
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_extension = os.path.basename(archive_path)
+        fn_no_extension = os.path.splitext(fn_with_extension)
+        deploy_path = "/data/web_static/releases/"
 
-        archive_name = os.path.basename(archive_path)
-        folder_name = os.path.splitext(archive_name)[0]
-
-        for host in env.hosts:
-            with Connection(host, user=env.user, connect_kwargs={"key_filename": env.key_filename}) as conn:
-                conn.put(archive_path, "/tmp/")
-                conn.run("mkdir -p /data/web_static/releases/{}".format(folder_name))
-                conn.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(archive_name, folder_name))
-                conn.run("rm /tmp/{}".format(archive_name))
-                conn.run("rm -rf /data/web_static/current")
-                conn.run("ln -s /data/web_static/releases/{} /data/web_static/current".format(folder_name))
-
+        put(archive_path, "/tmp/")
+        run(f"rm -p {deploy_path}{fn_no_extension}/")
+        run(f"mkdir -p {deploy_path}{fn_no_extension}/")
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            fn_with_extension, deploy_path, fn_no_extension))
+        run(f"rm /tmp/{fn_with_extension}")
+        run("mv {0}{1}/web_static/* {0}{1}/".format(deploy_path, fn_no_extension))
+        run(f"rm -rf {deploy_path}{fn_no_extension}/web_static")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {deploy_path}{fn_no_extension}/ /data/web_static/current")
+        print("Deployment complete.")
         return True
     except Exception as e:
         print(f"An error occurred during deployment: {e}")
         return False
-
-if __name__ == "__main__":
-    archive_path = do_pack()
-    if archive_path:
-        if do_deploy(archive_path):
-            print("Deployment successful.")
-        else:
-            print("Deployment failed.")
-    else:
-        print("Archive creation failed.")
