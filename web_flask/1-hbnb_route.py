727e1140d85841998c57cc03527c22b336c36d6d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-23 15:09:37 +0000

    add file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""flask module"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """print output"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """print output"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """set port and host"""
+    app.run(host='8.8.8.8', port=5000)
