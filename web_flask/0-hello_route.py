524e677fd364968dc597fad82861fe8fe9c28f57
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-23 14:40:58 +0000

    edit file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+"""flask module"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """print output"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    """set port and host"""
     app.run(host='0.0.0.0', port=5000)
