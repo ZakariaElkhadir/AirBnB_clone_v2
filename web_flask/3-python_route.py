2ed9eea140c1ebcbc0b308275be484371a0d081d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-24 15:28:35 +0000

    edit file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,18 +17,22 @@ def hello():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """print output"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
+    """print output"""
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_rooute(text="is cool"):
+    """print output"""
     return "Python {}".format(text.replace('_', ''))
 
 
 if __name__ == '__main__':
+    """set port and host"""
     app.run(host='0.0.0.0', port=5000)
