cd4f231b93224fcffb5fe603031fa6a273909989
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-24 17:17:04 +0000

    edit file

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -35,13 +35,13 @@ def number_template(n):
     """print output"""
     return render_template('5-number.html', number=n)
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """print output"""
-    if n % 2 == 0:
-        return render_template('6-number_odd_or_even.html', number=n, odd_even="even")
-    else:
-        return render_template('6-number_odd_or_even.html', number=n, odd_even="odd")
+    """display slag number"""
+    if isinstance(n, int):
+        is_even = n % 2 == 0
+        return render_template(
+            '6-number_odd_or_even.html', number=n, is_even=is_even)
 
 
 if __name__ == "__main__":
