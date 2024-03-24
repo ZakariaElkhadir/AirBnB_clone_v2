644703c5fa82b18a1cd8412a358fc49deb28a51f
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-24 16:53:36 +0000

    edit file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number_route(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """print output"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
