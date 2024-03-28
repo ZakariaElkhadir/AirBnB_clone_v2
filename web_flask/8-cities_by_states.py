58213461d07cf34f956ebe05496a561c72606b80
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-28 02:04:18 +0000

    edit file

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a HTML page with a list of states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def closer(exception):
+    """Closes the current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
