65527f46f96fe4854bea8fc54eb12a82d140ac54
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-03-28 02:32:03 +0000

    edit file

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Starts a Flask web application"""
+import os
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays a HTML page with a list of states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """displays a HTML page with a list of states"""
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('9-states.html', states=states, cities=cities, id=id)
+
+@app.teardown_appcontext
+def closer(exception):
+    """Closes the current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
